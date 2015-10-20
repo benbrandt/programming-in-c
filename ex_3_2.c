1237f2a4c361ffbf0f4e3f407d6c64ca84e6689b
Ben Brandt <benjamin.j.brandt@gmail.com>
2015-10-20 11:53:34 -0700

    Exercise 3.2

---
+++ ex_3_2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (void)
+{
+    printf ("1. In C, lowercase letters are significant.\n"
+            "2. main is where program execution begins.\n"
+            "3.Opening and closing braces enclose program statements in a routine.\n"
+            "4. All program statements must be terminated by a semicolon.\n");
+    
+    return 0;
+}
