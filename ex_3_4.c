aee954bf721930de20156305b54c0fa4df316bb8
Ben Brandt <benjamin.j.brandt@gmail.com>
2015-10-20 12:00:29 -0700

    Exercise 3.4

---
+++ ex_3_4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (void)
+{
+    int value1, value2, sum;
+    
+    value1 = 87;
+    value2 = 15;
+    sum = value1 - value2;
+    printf ("The difference of %i and %i is %i\n", value1, value2, sum);
+    
+    return 0;
+}
