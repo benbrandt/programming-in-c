96c419ad819729b22e17425598752913d4ae24e3
Ben Brandt <benjamin.j.brandt@gmail.com>
2015-10-21 13:13:21 -0700

    Exercise 4.2

---
+++ chapter-4/ex_4_2.c
@@ -0,0 +1,21 @@
+// Basic conversion in C
+
+#include <stdio.h>
+
+int main (void)
+{
+    int Int; // Int is different than int
+    // int char; Invalid because it is a type
+    // int 6_05; Variables can't start with numbers
+    int Calloc;
+    int Xx;
+    int alpha_beta_routine;
+    int floating; // Not float
+    int _1312;
+    int z;
+    int ReInitialize;
+    int _;
+    int A$;
+    
+    return 0;
+}
