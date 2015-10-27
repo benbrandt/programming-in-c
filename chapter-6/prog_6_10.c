e8989e69ce25e4244e05b156379f08ce62282dba
Ben Brandt <benjamin.j.brandt@gmail.com>
2015-10-26 22:52:48 -0400

    Program 6.10: Revising the program to Generate a table of prime numbers

--- chapter-6/prog_6_10.c
+++ chapter-6/prog_6_10.c
@@ -1,20 +1,21 @@
 // Program to generate a table of prime numbers
 
 #include <stdio.h>
+#include <stdbool.h>
 
 int main (void)
 {
     int     p, d;
-    _Bool   isPrime;
+    bool   isPrime;
     
     for (p = 2; p <= 50; ++p) {
-        isPrime = 1;
+        isPrime = true;
         
         for (d = 2; d < p; ++d)
             if (p % d == 0)
-                isPrime = 0;
+                isPrime = false;
             
-        if (isPrime != 0)
+        if (isPrime != false)
             printf ("%i ", p);
     }
     
