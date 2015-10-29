1a5dfbe4a1fb63d64eaf7a28b214b2a59ce4cf0c
Ben Brandt <benjamin.j.brandt@gmail.com>
2015-10-29 12:51:57 -0700

    Program 8.13A: Multidimensional Variable-Lenght Arrays

--- chapter-8/prog_8_13.c
+++ chapter-8/prog_8_13.c
@@ -2,8 +2,9 @@
 
 int main (void)
 {
-    void    scalarMultiply (int matrix[3][5], int scalar);
-    void    displayMatrix (int matrix[3][5]);
+    void    scalarMultiply (int nRows, int nCols,
+                            int matrix[nRows][nCols], int scalar);
+    void    displayMatrix (int nRows, int nCols, int matrix[nRows][nCols]);
     int     sampleMatrix[3][5] = 
             {
                 {  7, 16, 55, 13, 12 },
@@ -12,40 +13,41 @@ int main (void)
             };
             
     printf ("Original matrix:\n");
-    displayMatrix (sampleMatrix);
+    displayMatrix (3, 5, sampleMatrix);
     
-    scalarMultiply (sampleMatrix, 2);
+    scalarMultiply (3, 5, sampleMatrix, 2);
     
     printf ("\nMultiplied by 2:\n");
-    displayMatrix (sampleMatrix);
+    displayMatrix (3, 5, sampleMatrix);
     
-    scalarMultiply (sampleMatrix, -1);
+    scalarMultiply (3, 5, sampleMatrix, -1);
     
     printf ("\nThen multiplied by -1:\n");
-    displayMatrix (sampleMatrix);
+    displayMatrix (3, 5, sampleMatrix);
     
     return 0;
 }
 
 // Function to multiply a 3 x 5 array by a scalar
 
-void scalarMultiply (int matrix[3][5], int scalar)
+void scalarMultiply (int nRows, int nCols,
+                     int matrix[nRows][nCols], int scalar)
 {
     int row, column;
     
-    for (row = 0; row < 3; ++row)
-        for (column = 0; column < 5; ++column)
+    for (row = 0; row < nRows; ++row)
+        for (column = 0; column < nCols; ++column)
             matrix[row][column] *= scalar;
 }
 
 // Function to display a 3 x 5 array
 
-void displayMatrix (int matrix[3][5])
+void displayMatrix (int nRows, int nCols, int matrix[nRows][nCols])
 {
     int row, column;
     
-    for (row = 0; row < 3; ++row) {
-        for (column = 0; column < 5; ++ column)
+    for (row = 0; row < nRows; ++row) {
+        for (column = 0; column < nCols; ++ column)
             printf ("%5i", matrix[row][column]);
         
         printf("\n");
