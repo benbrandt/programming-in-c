5b4191c5a934a7af3cea33e7a88af2b32ac78569
Ben Brandt <benjamin.j.brandt@gmail.com>
2015-11-17 12:38:30 -0800

    Exercise 10.4

--- chapter-10/ex_10_4.c
+++ chapter-10/ex_10_4.c
@@ -2,6 +2,13 @@
 
 int main (void)
 {
+    void substring (const char source[], int start, int count, char result[]);
+    
+    char result[80];
+    
+    substring("two words", 4, 20, result);
+    
+    printf ("%s\n", result);
     
     return 0;
 }
@@ -10,11 +17,11 @@ void substring (const char source[], int start, int count, char result[])
 {
     int i, j;
     
-    for (i = start, j = 0; i <= count; ++i)
+    for (i = start, j = 0; source[i] != '\0' && j <= count; ++i)
     {
         result[j] = source[i];
         j++;
     }
     
-    
+    result[j] = '\0';
 }
